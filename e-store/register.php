51817377af46715a04dd8e18a489efadf5edb676
pavel-mitev <pavel.mitev23@gmail.com>
2018-07-09 17:50:30 +0300

    Adding files

--- e-store/register.php
+++ e-store/register.php
@@ -19,54 +19,58 @@
       </ul>
     </div>
   </nav>
-
     <div class="container">
-      <h3>Registration Form</h3>
+      <legend style="font-size:30px;">Registration</legend>
     </div>
 
+    <?php
+        if($_GET["userEmpty"] || $_GET["emailEmpty"] || $_GET["firstnameEmpty"] || $_GET["lastnameEmpty"] || $_GET["passEmpty"] || $_GET["repeatPassEmpty"] || $_GET["notMatching"])
+          echo "<div class='alert alert-danger'> <strong>Filling all fields in the form is required.</strong> </div>";
+
+    ?>
     <form action="./register_authentication.php" method="post">
     <div class="container">
       <div class="form-group">
-        <div class="col-xs-6">
+        <div class="col-xs-6"  id="userField">
             <label for="usr">Username</label>
-            <input type="text" class="form-control" id="usr" name="usr">
+            <input type="text" class="form-control" id="usr" name="usr" maxlength="50">
         </div>
       </div>
     </div>
 
     <div class=container>
       <div class="form-group">
-        <div class="col-xs-6">
+        <div class="col-xs-6" id="emailField">
           <label for="email">Email</label>
-          <input type="email" class="form-control" id="email" name="email">
+          <input type="email" class="form-control" id="email" name="email" maxlength="50">
         </div>
       </div>
     </div>
 
     <div class="container">
       <div class="form-group">
-        <div class="col-xs-3">
+        <div class="col-xs-3" id="firstnameField">
             <label for="firstname">First Name</label>
-            <input type="text" class="form-control" id="firstname" name="firstname">
+            <input type="text" class="form-control" id="firstname" name="firstname" maxlength="50">
         </div>
-        <div class="col-xs-3">
+        <div class="col-xs-3" id="lastnameField">
           <label for="lastname">Last Name</label>
-          <input type="text" class="form-control" id="lastname" name="lastname">
+          <input type="text" class="form-control" id="lastname" name="lastname" maxlength="50">
         </div>
       </div>
     </div>
 
     <div class="container">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-6" id="pwdField">
         <label for="pwd">Password</label>
-        <input type="password" class="form-control" id="pwd" name="pwd">
+        <input type="password" class="form-control" id="pwd" name="pwd" maxlength="50">
       </div>
     </div>
 
     <div class="container">
-      <div class="form-group col-xs-6">
+      <div class="form-group col-xs-6" id="repeatpwdField">
         <label for="repeatpwd">Re-enter Password</label>
-        <input type="password" class="form-control" id="repeatpwd" name="repeatpwd">
+        <input type="password" class="form-control" id="repeatpwd" name="repeatpwd" maxlength="50">
       </div>
     </div>
     <div class="container">
@@ -75,5 +79,105 @@
       </div>
     <div>
     </form>
+
+    <script>
+      var userField = document.getElementById("userField");
+      var userInput = document.getElementById("usr");
+      userInput.onblur = function(){
+          if(userField.childElementCount == 2 && userInput.value == ""){
+            userInput.style.borderColor = "red";
+            appendErrMsg("Username is required", userField);
+          }else if(userField.childElementCount > 2 && userInput.value != ""){
+            userField.removeChild(userField.children[2]);
+            userInput.style.borderColor = "";
+          }
+        }
+
+      var emailField = document.getElementById("emailField");
+      var emailInput = document.getElementById("email");
+      emailInput.onblur = function(){
+        var emailRegEx = /.+@.+\..+/i;
+
+        if(emailField.childElementCount == 2 && !emailRegEx.test(emailInput.value)){
+            emailInput.style.borderColor = "red";
+            appendErrMsg("Valid email address is required",emailField);
+        }else if(emailField.childElementCount > 2 && emailRegEx.test(emailInput.value)){
+          emailField.removeChild(emailField.children[2]);
+          emailInput.style.borderColor = "";
+        }
+
+      }
+
+      var firstnameField = document.getElementById("firstnameField");
+      var firstnameInput = document.getElementById("firstname");
+
+      firstnameInput.onblur = function(){
+        if(firstnameField.childElementCount == 2 && firstnameInput.value == ""){
+          firstnameInput.style.borderColor = "red";
+          appendErrMsg("First name is required",firstnameField);
+
+        }else if(firstnameField.childElementCount > 2 && firstnameInput.value != ""){
+          firstnameField.removeChild(firstnameField.children[2]);
+          firstnameInput.style.borderColor = "";
+        }
+      }
+
+      var lastnameField = document.getElementById("lastnameField");
+      var lastnameInput = document.getElementById("lastname");
+
+      lastnameInput.onblur = function(){
+
+        if(lastnameField.childElementCount == 2 && lastnameInput.value == ""){
+          lastnameInput.style.borderColor = "red";
+          appendErrMsg("Last name is required",lastnameField);
+        }else if(lastnameField.childElementCount > 2 && lastnameInput.value != ""){
+          lastnameField.removeChild(lastnameField.children[2]);
+          lastnameInput.style.borderColor = "";
+        }
+      }
+
+
+      var passwordField = document.getElementById("pwdField");
+      var passwordInput = document.getElementById("pwd");
+
+      passwordInput.onblur = function(){
+        passwordRegEx = /.{6,}/;
+        if(passwordField.childElementCount == 2 && !passwordRegEx.test(passwordInput.value)){
+          passwordInput.style.borderColor = "red";
+          appendErrMsg("Password must be atleast 6 characters long",passwordField);
+        }else if(passwordField.childElementCount > 2 && passwordRegEx.test(passwordInput.value)){    
+          passwordField.removeChild(passwordField.children[2]);
+          passwordInput.style.borderColor = "";
+        }
+      }
+
+
+      var repeatpwdField = document.getElementById("repeatpwdField");
+      var repeatpwdInput = document.getElementById("repeatpwd");
+
+      repeatpwdInput.onblur = function(){
+        if(repeatpwdField.childElementCount == 2 && (passwordInput.value != repeatpwdInput.value || repeatpwdInput.value == "")){
+          repeatpwdInput.style.borderColor = "red";
+          appendErrMsg("Passwords do not match",repeatpwdField);
+        }else if(repeatpwdField.childElementCount > 2 && (passwordInput.value == repeatpwdInput.value) && repeatpwdInput.value != ""){
+          repeatpwdField.removeChild(repeatpwdField.children[2]);
+          repeatpwdInput.style.borderColor = "";
+        }
+      }
+
+
+
+
+
+      function appendErrMsg(errorMsg,elem){
+        var p = document.createElement("P");
+        p.style.color = "red";
+        var t = document.createTextNode(errorMsg);
+        p.appendChild(t);
+        elem.appendChild(p);
+      }
+
+
+    </script>
 <body>
 </html>
--- e-store/register_authentication.php
+++ e-store/register_authentication.php
@@ -3,6 +3,31 @@
 require './database_connection.php';
 
 
+if(!$_POST['usr'])
+	die(header("location:register.php?userEmpty=1"));
+if(!$_POST['email'])
+	die(header("location:register.php?emailEmpty=1"));
+if(!$_POST['firstname'])
+	die(header("location:register.php?firstnameEmpty=1"));
+if(!$_POST['lastname'])
+	die(header("location:register.php?lastnameEmpty=1"));
+if(!$_POST['pwd'])
+	die(header("location:register.php?passEmpty=1"));
+if(!$_POST['repeatpwd'])
+	die(header("location:register.php?repeatPassEmpty=1"));
+if($_POST['repeatpwd'] != $_POST['pwd'])
+	die(header("location:register.php?notMatching=1"));
 
+echo $_POST['repeatpwd'] . " " . $_POST['pwd'];
+
+$username = $_POST['usr'];
+$email = $_POST['email'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$password = $_POST['password'];
+
+if(!validateEmail($email)){
+	die(header("location:register.php?invalidEmail=1"))
+}
 mysqli_close($connect);
 ?>
